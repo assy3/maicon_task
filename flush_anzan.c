eedbf9d3f3564e900cffe811bddf67c1087c6795
assy3 <33861939+assy3@users.noreply.github.com>
2020-01-20 16:35:30 +0900

    タイトル画面追加

--- flush_anzan.c
+++ flush_anzan.c
@@ -17,7 +17,7 @@ volatile unsigned char sw, sw_flag;
 volatile unsigned char bz_conf;
 unsigned char led[8];
 
-unsigned char pat[12][8] = {
+unsigned char pat[13][8] = {
  {7, 5, 5, 5, 7, 0, 0, 0}, // 0
  {1, 1, 1, 1, 1, 0, 0, 0}, // 1
  {7, 1, 7, 4, 7, 0, 0, 0}, // 2
@@ -29,7 +29,8 @@ unsigned char pat[12][8] = {
  {7, 5, 7, 5, 7, 0, 0, 0}, // 8
  {7, 5, 7, 1, 7, 0, 0, 0}, // 9
  {0, 0, 0, 0, 0, 8, 0, 0}, // .
- {0, 0, 0, 0, 0, 0, 7, 0}, // _
+ {0, 0, 0, 0, 0, 0, 7, 0}, // 
+ {0, 0, 32, 56, 60, 56, 32, 0},
  };
 
 void update_led()
@@ -108,7 +109,13 @@ int main(){
 	TIMSK1 |= _BV(OCIE1A);
 	
 	sei();
+
+	// タイトル画面	
+	for(i = 0; i < 8; i++){
+		led[i] = pat[12][i];						
+	}
 	
+
 	for(;;){
 		wdt_reset();
 		cnt++;
@@ -120,6 +127,9 @@ int main(){
 					j += 1;
 					sum += n;
 					for(i = 0; i < 7; i++){
+						if(led[i + 1] != 0){
+							led[i + 1] = 0;
+						}
 						led[i + 1] = (pat[n / 10][i] << 4 )| pat[n % 10][i];						
 					}
 					bz_conf = 1;
@@ -127,12 +137,16 @@ int main(){
 				}				
 			}			
 		}
+
 		if(sw_flag & 2){
 			sw_flag = 0;
 			result_hand = sum / 100;
 			result_ltwd = sum % 100;
 			
 			for(i = 0; i < 7; i++){
+				if(led[i + 1] != 0){
+					led[i + 1] = 0;
+				}
 				if(result_hand == 0){
 					led[i + 1] = (pat[sum / 10][i] << 4) | pat[sum % 10][i];
 				}
@@ -160,6 +174,8 @@ int main(){
 			bz_conf = 1;
 			TCCR1B = 0x0C;
 		}
+
+			
 	}	
 	return 0;
 }
